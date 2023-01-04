ebae1eaf7e286077e74ca03cf159192f0e27dfd7
mark <markpackham1@gmail.com>
2023-01-04 09:40:01 +0000

    add try catch statement

--- Program.cs
+++ Program.cs
@@ -6,14 +6,16 @@
         {
 
 
-                Random rnd = new Random();
+            Random rnd = new Random();
 
-                int num1 = rnd.Next(1, 9);
-                int num2 = rnd.Next(1, 9);
-                int answer = num1 * num2;
-                Console.WriteLine($"{num1} * {num2} = what?");
+            int num1 = rnd.Next(1, 9);
+            int num2 = rnd.Next(1, 9);
+            int answer = num1 * num2;
+            Console.WriteLine($"{num1} * {num2} = what?");
+
+            try
+            {
                 int guess = Convert.ToInt32(Console.ReadLine());
-
                 if (guess == answer)
                 {
                     Console.WriteLine("Correct");
@@ -22,9 +24,11 @@
                 {
                     Console.WriteLine($"Wrong answer was {answer}");
                 }
-
-
+            }
+            catch
+            {
+                Console.WriteLine("Int32 values only");
+            }
         }
-
     }
 }
