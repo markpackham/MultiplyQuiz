00fde97729b1e69d038799be3938174eaecb2011
mark <markpackham1@gmail.com>
2023-01-04 09:54:36 +0000

    add exit for app

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
-﻿namespace MultiplyQuiz
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace MultiplyQuiz
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void multiplyIt()
         {
-
-
             Random rnd = new Random();
 
-            int num1 = rnd.Next(1, 9);
-            int num2 = rnd.Next(1, 9);
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 100);
             int answer = num1 * num2;
-            Console.WriteLine($"{num1} * {num2} = what?");
+            Console.WriteLine($"{num1} * {num2} = what? Press 0 to quit.");
 
             try
             {
                 int guess = Convert.ToInt32(Console.ReadLine());
-                if (guess == answer)
+                if(guess == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else if (guess == answer)
                 {
                     Console.WriteLine("Correct");
                 }
@@ -29,6 +34,14 @@
             {
                 Console.WriteLine("Int32 values only");
             }
+
+            multiplyIt();
+        }
+
+
+        static void Main(string[] args)
+        {
+            multiplyIt();
         }
     }
 }
