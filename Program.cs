89815fb2af114e245c2718c26d89209436419373
mark <markpackham1@gmail.com>
2023-01-04 09:34:39 +0000

    C# Multiply Quiz

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MultiplyQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                Random rnd = new Random();
+
+                int num1 = rnd.Next(1, 9);
+                int num2 = rnd.Next(1, 9);
+                int answer = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = what?");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == answer)
+                {
+                    Console.WriteLine("Correct");
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong answer was {answer}");
+                }
+
+
+        }
+
+    }
+}
