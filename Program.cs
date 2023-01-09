099c55fda4c8c3a4f62887ab15cbd25b850c5ae5
mark <markpackham1@gmail.com>
2023-01-09 09:49:29 +0000

    let user supply a power

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace MultiplyQuiz
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Choose *, +, -, square, squareR or /. The default is *");
+            Console.WriteLine("Choose *, +, -, /, square, squareR or power. The default is *");
             string choice = Console.ReadLine().ToString();
 
             if (choice.Equals("+"))
@@ -22,6 +22,21 @@ namespace MultiplyQuiz
             else if (choice.Equals("/"))
             {
                 DivideOper.divideIt();
+            }
+            else if (choice.Equals("power"))
+            {
+                Console.WriteLine("Supply a power value that is an Int32, 3 is the default");
+                try
+                {
+                    int pow = Convert.ToInt32(Console.ReadLine());
+                    PowerOper.powerIt(pow);
+                }
+                catch
+                {
+                    int pow = 3;
+                    PowerOper.powerIt(pow);
+                }
+                
             }
             else if (choice.Equals("square"))
             {
---
+++ operators/PowerOper.cs
@@ -0,0 +1,33 @@
+﻿using MultiplyQuiz.helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyQuiz.operators
+{
+    public class PowerOper
+    {
+        public static void powerIt(int pow)
+        {
+            Random rnd = new Random();
+
+            int num1 = rnd.Next(1, 10);
+            int answer = (int)Math.Pow(num1, pow);
+            Console.WriteLine($"{num1} to the power of {pow} = what? Press 0 to quit.");
+
+            try
+            {
+                CheckUserInput.checkUserInput(answer);
+            }
+            catch
+            {
+                Console.WriteLine($"Int32 values only, answer was {answer}");
+                powerIt(pow);
+            }
+
+            powerIt(pow);
+        }
+    }
+}
